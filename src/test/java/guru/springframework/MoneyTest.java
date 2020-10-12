f30b714cb6d03fa252eb1a3c4c201bccf256c58d
Levi Martines <levi.mferreira@gmail.com>
2020-10-12 18:51:18 -0300

    Teste de igualdade implementado para falhar

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -8,11 +8,18 @@ public class MoneyTest {
 
 	@Test
 	void testMultiplication() {
+
 		Dollar five = new Dollar(5);
 		Dollar product = five.times(2);
 		assertEquals(10, product.getAmount());
 
 		product = five.times(3);
 		assertEquals(15, product.getAmount());
+	}
+
+	@Test
+	void testEquality() {
+
+		assertEquals(new Dollar(5), new Dollar(5));
 	}
 }
