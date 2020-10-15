140fa4fc5424d155f1385c8016457329ccee1cf3
Levi Martines <levi.mferreira@gmail.com>
2020-10-15 10:45:08 -0300

    Interesting Times - Código das classes Dollar e Franc refatorados para a Money

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public abstract class Money {
+public class Money {
 
 	protected int amount;
 	protected String currency;
@@ -22,12 +22,21 @@ public abstract class Money {
 		return currency;
 	}
 
-	public abstract Money times(int multiplier);
+	public Money times(int multiplier) {
+		return new Money(amount * multiplier, this.currency);
+	}
 
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount &&
-			this.getClass().equals(object.getClass());
+			this.currency.equals(((Money) object).currency);
 	}
 
+	@Override
+	public String toString() {
+		return "Money{" +
+			"amount=" + amount +
+			", currency='" + currency + '\'' +
+			'}';
+	}
 }
